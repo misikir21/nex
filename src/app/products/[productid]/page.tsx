5f77b5278d550e8f907413f69ecc974818068e04
misikir21 <misikirkaka@gmail.com>
2024-02-09 15:41:08 +0300

    remove the product page

--- src/app/products/[productid]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function product({ params }: { params: { productid: string } }) {
-  return <h1>this is a detail page about{params.productid}</h1>;
-}
-
-export default product;
